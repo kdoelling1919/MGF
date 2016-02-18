a2f63b8a1db11f72161cee831d8b48107b479557
Keith Doelling <kd889@nyu.edu>
2016-02-18 16:39:29 -0500

    Initial preprocessing (largely) complete.

--- Ph_runanalysis.m
+++ Ph_runanalysis.m
@@ -4,6 +4,7 @@ sqdfile = [subdir 'R0013_Phonetica1-LSdenoised_NR.sqd'];
 trialdef = struct('trig',164:167,'prestim',0,'poststim',5,'offset',-1);
 trialfun = 'alltrialfun';
 samplefs = 500;
+ncomps = 32;
 
 [data,trlinfo,layout,neighbours] = Ph_meganalysis(sqdfile,trialdef,trialfun,samplefs);
 
@@ -12,7 +13,7 @@ data = Ph_cleanbadchans(data,layout,neighbours,seglength);
 
 pcaname=regexp(sqdfile,'R\d{3,4}_(?<name>[a-zA-Z0-9]+)','names');
 pcafile = [subdir pcaname.name '_PCA.jpg'];
-[ft_ica,ft_pca,weights,sphere] = Ph_megica(data,layout,ncomps,pcafile,[]);
+[ft_ica,ft_pca,weights,sphere] = Ph_megica(data,layout,ncomps,pcafile,false);
 
 cleanchdata = Ph_cleanica(ft_ica,ft_pca,weights,sphere,layout);
 
