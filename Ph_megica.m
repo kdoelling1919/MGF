7ef4a5c4b49c1bd8cfad3c71478d573a496ec8c0
Keith Doelling <kd889@nyu.edu>
2016-02-17 19:14:59 -0500

    [MOD] Updated function Ph_megica.m to work more smoothly and allow for the option to remove 1st PCA component.

--- Ph_megica.m
+++ Ph_megica.m
@@ -1,42 +1,72 @@
-function ft_ICA = Ph_megica(data,layout,ncomps)
+function ft_ICA = Ph_megica(data,layout,ncomps,pcafile,remove1)
+%{      
+    Runs ICA on a PCA projection of MEG data in fieldtrip format.
+        data    = (struct) data struct output from ft_preprocessing
+        layout  = (struct) layout struct output from ft_prepare_layout
+        ncomps  = (double) number of PCA components to use for ICA
+        pcafile = (string) filename to save figure of first 10 PCA projections (if
+            empty, no figure will be saved)
+        remove1 = (logical) remove first PCA component? if empty, it will
+            prompt for answer after showing it you the projection
+ 
+    Code adapted from Adeen Flinker, by Keith Doelling
+%}
+     % demean the data   
+    data.trial{1} = data.trial{1} - repmat(mean(data.trial{1},2),1,size(data.trial{1},2));
+    % get eigen vectors for PCA
+    [EigenVectors]=pcsquash(data.trial{1});
+    % PCA components
+    ft_PCA = ft_componentanalysis(struct('demean','no','unmixing',EigenVectors','topolabel',{data.label}),data);
+    
+    % PCA
+    cfg = [];
+    cfg.layout = layout; % specify the layout file that should be used for plotting
+    cfg.viewmode = 'component';
+    cfg.continous = 'yes';
+    cfg.compscale = 'local';
+    cfg.blocksize = 20;
+    cfg.ylim = [-4 4].*std(ft_PCA.trial{1}(:));
 
-        data.trial{1} = data.trial{1} - repmat(mean(data.trial{1},2),1,size(data.trial{1},2));
-        [EigenVectors]=pcsquash(data.trial{1});
-        
-        ft_PCA = ft_componentanalysis(struct('demean','no','unmixing',EigenVectors','topolabel',{data.label}),data);
-
-         cfg = [];
-        cfg.layout = layout; % specify the layout file that should be used for plotting
-        cfg.viewmode = 'component';
-        cfg.continous = 'yes';
-        cfg.compscale = 'local';
-        cfg.blocksize = 20;
-        cfg.ylim = [-4 4].*std(ft_PCA.trial{1}(:));
-        
-        ft_databrowser(cfg, ft_PCA);
-        saveas(gcf,['PCA_twotones' num2str(i)],'jpg')
-        close(gcf)
-        
-        % Automatically remove 1st PCA component and get ICA 
-        
-        cfg = [];
-        cfg.demean = 'no';
+    ft_databrowser(cfg, ft_PCA);
+    drawnow
+    if ~isempty(pcafile)
+        saveas(gcf,pcafile,'jpg')
+    end
+    close(gcf)
+    
+    % prepare to reject components
+    cfg = [];
+    cfg.demean = 'no';
+    % if remove1 is empty then ask for input
+    if isempty(remove1)
+        reply = input('Remove first component? [y/n]: ','s');
+        if strcmp(reply,'y')
+            remove1 = true;
+        else
+            remove1 = false;
+        end
+    end
+    % get rid of component or don't
+    if remove1
         cfg.component = 1;
-        PCA_postreject = ft_rejectcomponent(cfg,ft_PCA);
-        clear ft_PCA
-        PCA_postreject.trial{1} = PCA_postreject.trial{1} - repmat(mean(PCA_postreject.trial{1},2),1,size(PCA_postreject.trial{1},2));
-        [EigenVectors]=pcsquash(PCA_postreject.trial{1});            % pca eigenvectors from eeglab
-        ft_PCAreject1 = ft_componentanalysis(struct('demean','no','unmixing',EigenVectors','topolabel',{PCA_postreject.label}),PCA_postreject);
+    else
+        cfg.component = [];
+    end
+    PCA_postreject = ft_rejectcomponent(cfg,ft_PCA);
+    clear ft_PCA
+    PCA_postreject.trial{1} = PCA_postreject.trial{1} - repmat(mean(PCA_postreject.trial{1},2),1,size(PCA_postreject.trial{1},2));
+    [EigenVectors]=pcsquash(PCA_postreject.trial{1});            % pca eigenvectors from eeglab
+    ft_PCAreject1 = ft_componentanalysis(struct('demean','no','unmixing',EigenVectors','topolabel',{PCA_postreject.label}),PCA_postreject);
 %       
-        [weights, sphere]=runica(ft_PCAreject1.trial{1}(1:ncomps,:),'lrate',0.001);
+    [weights, sphere]=runica(ft_PCAreject1.trial{1}(1:ncomps,:),'lrate',0.001);
 
-        dummy = ft_PCAreject1; dummy.trial{1} = ft_PCAreject1.trial{1}(1:ncomps,:); dummy.topo = dummy.topo(1:ncomps,1:ncomps);
-        dummy.label = dummy.label(1:ncomps); dummy.topolabel = dummy.topolabel;
+    dummy = ft_PCAreject1; dummy.trial{1} = ft_PCAreject1.trial{1}(1:ncomps,:); dummy.topo = dummy.topo(1:ncomps,1:ncomps);
+    dummy.label = dummy.label(1:ncomps); dummy.topolabel = dummy.topolabel;
 
-        ft_ICA = ft_componentanalysis(struct('demean','no','unmixing',weights*sphere,'topolabel',{dummy.label}),dummy);
+    ft_ICA = ft_componentanalysis(struct('demean','no','unmixing',weights*sphere,'topolabel',{dummy.label}),dummy);
 
-        % change topography data to match ica + pca transformation
-        ft_ICA.topolabel_orig = ft_ICA.topolabel;
-        ft_ICA.topolabel = ft_PCAreject1.topolabel;
-        ft_ICA.topo = pinv(weights*sphere*EigenVectors(:,1:ncomps)'*eye(length(EigenVectors)));
+    % change topography data to match ica + pca transformation
+    ft_ICA.topolabel_orig = ft_ICA.topolabel;
+    ft_ICA.topolabel = ft_PCAreject1.topolabel;
+    ft_ICA.topo = pinv(weights*sphere*EigenVectors(:,1:ncomps)'*eye(length(EigenVectors)));
 end
--- Ph_runanalysis.m
+++ Ph_runanalysis.m
@@ -5,7 +5,10 @@ trialdef = struct('trig',164:167,'prestim',0,'poststim',5,'offset',-1);
 trialfun = 'alltrialfun';
 
 [data,trlinfo,layout,neighbours] = Ph_meganalysis(sqdfile,trialdef,trialfun);
-ica = Ph_megica(data,layout,ncomps);
+pcaname=regexp(sqdfile,'R\d{3,4}_(?<name>[a-zA-Z0-9]+)','names');
+
+pcafile = [subdir pcaname.name '_PCA.jpg'];
+ica = Ph_megica(data,layout,ncomps,pcafile,[]);
 
 mkdir([subdir 'Processed']);
 savefile = [subdir 'Processed/variables.mat'];
