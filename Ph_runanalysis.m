056c07fec6bbe46baa3e3096e48fa688c660c197
Keith Doelling <kd889@nyu.edu>
2016-02-18 17:17:39 -0500

    [ENH] Ph_runanalysis iterate over subjects and Noise-Reduced Files

--- Ph_runanalysis.m
+++ Ph_runanalysis.m
@@ -1,26 +1,35 @@
-subdir = '/Volumes/Vault/Data/Phonetica/R0013/';
+homedir = '/Volumes/Vault/Data/Phonetica';
+% find subjects
+subjects = dir([homedir '/R*']);
+srch = '*Phonetica*NR.sqd';
 
-sqdfile = [subdir 'R0013_Phonetica1-LSdenoised_NR.sqd'];
-trialdef = struct('trig',164:167,'prestim',0,'poststim',5,'offset',-1);
-trialfun = 'alltrialfun';
-samplefs = 500;
-ncomps = 32;
+for sub = 1:length(subjects)
+    subdir = [homedir '/' subjects(sub).name];
+    sqdfiles = dir([subdir '/' srch]);
+    
+    for sqd = 1:length(sqdfiles)
+        sqdfile = [subdir '/' sqdfiles(sqd).name];
+        trialdef = struct('trig',164:167,'prestim',0,'poststim',5,'offset',-1);
+        trialfun = 'alltrialfun';
+        samplefs = 500;
+        ncomps = 32;
 
-[data,trlinfo,layout,neighbours] = Ph_meganalysis(sqdfile,trialdef,trialfun,samplefs);
+        [data,trlinfo,layout,neighbours] = Ph_meganalysis(sqdfile,trialdef,trialfun,samplefs);
 
-seglength = numSubPlot(length(data.trial{1})./data.fsample);
-data = Ph_cleanbadchans(data,layout,neighbours,seglength);
+        seglength = numSubPlot(length(data.trial{1})./data.fsample);
+        data = Ph_cleanbadchans(data,layout,neighbours,seglength);
 
-pcaname=regexp(sqdfile,'R\d{3,4}_(?<name>[a-zA-Z0-9]+)','names');
-pcafile = [subdir pcaname.name '_PCA.jpg'];
-[ft_ica,ft_pca,weights,sphere] = Ph_megica(data,layout,ncomps,pcafile,false);
+        pcaname=regexp(sqdfile,'R\d{3,4}_(?<name>[a-zA-Z0-9]+)','names');
+        pcafile = [subdir pcaname.name '_PCA.jpg'];
+        [ft_ica,ft_pca,weights,sphere] = Ph_megica(data,layout,ncomps,pcafile,false);
 
-cleanchdata = Ph_cleanica(ft_ica,ft_pca,weights,sphere,layout);
+        cleanchdata = Ph_cleanica(ft_ica,ft_pca,weights,sphere,layout);
 
-mkdir([subdir 'Processed']);
-savefile = [subdir 'Processed/variables.mat'];
-save(savefile,'cleanchdata','data','trlinfo','layout','neighbours','ft_ica','ft_pca','weights','sphere','-v7.3');
-
+        mkdir([subdir 'Processed']);
+        savefile = [subdir 'Processed/' pcaname.name '.mat'];
+        save(savefile,'cleanchdata','data','trlinfo','layout','neighbours','ft_ica','ft_pca','weights','sphere','-v7.3');
+    end
+end
 %% remove ica components and clear
 
 
