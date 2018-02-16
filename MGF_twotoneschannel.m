bbf9b6fd7dc1cb7ab86100e2d2d1f203b118eaee
Keith Doelling <kd889@nyu.edu>
2018-02-16 13:40:37 -0500

    Develop functions to deal easily with two tone data and get out best channels.

---
+++ MGF_twotoneschannel.m
@@ -0,0 +1,38 @@
+function [ bestchannels ] = MGF_twotoneschannel( filename, triggers, nchans, plotflag)
+%MGF_twotonechannels reads in denoised MEG data and outputs best channels
+%for two tone data by finding the peak M100 respond and 
+%   Detailed explanation goes here
+
+    if ~exist(filename, 'file')
+        error('File does not exist')
+    end
+    if nargin < 2
+        triggers = {161:162};
+    end
+    if nargin < 3
+        nchans = 10;
+    end
+    if ~iscell(triggers)
+        triggers = {triggers};
+    end
+    
+    trlinfo = MGF_triggerread(filename,triggers,1,1);
+    [data, layout, neighbours, trlinfo] = MGF_meganalysis(filename, [], 100, 20, 1, trlinfo);
+    
+    [ica,pca,weights,sphere] = MGF_megica(data, layout, 32, [], 0);
+    % remove ica components you don't like
+    cleandata = MGF_cleanica(ica,pca,weights,sphere,layout);
+    % remove any other clearly unruly channels this should be limited to a
+    % few and mostly to recovering dead channels. The next step will fix
+    % jumps over thresholds and the like
+    cleandata = MGF_cleanbadchans(cleandata, layout, neighbours, [], 'visual'); % sometimes generates issues with data.grad not having the right size
+    cleandata = MGF_overclean(cleandata, neighbours, 3e-12, 10, .05, 50);
+    
+    % epoch data;
+    cfg = [];
+    cfg.trl = trlinfo.trl;
+    cleandata = ft_redefinetrial(cfg, cleandata);
+    
+    bestchannels = MGFchannelselect(cleandata, layout, [.07 .14], nchans, 0, plotflag, [-.1 .3]);
+end
+
---
+++ MGFchannelselect.m
@@ -0,0 +1,57 @@
+function [ bestchannels ] = MGFchannelselect( data, layout, timewin, nchans, findtime, plotflag, plotwin)
+%MGFchannelselect Select best channels based on peak response in a
+%timewindow
+%   Detailed explanation goes here
+
+    cfg = [];
+    cfg.covariance = 'no';
+    cfg.keeptrials = 'no';
+    avg = ft_timelockanalysis(cfg,data);
+    
+    minlim = timewin(1);
+    minind = nearest(avg.time,minlim);
+    maxlim = timewin(2);
+    maxind = nearest(avg.time,maxlim);
+    
+    % find average peak moment and select channels sorted at this timepoint
+    if findtime
+        avg.rms = rms(avg.avg);
+        [~,j] = max(avg.rms(minind:maxind));
+        j = j + minind - 1;
+        fprintf('Peak of data found at %f s.', avg.time(j));    
+        [~,chans] = sort(mean(abs(avg.avg(:,j-1:j+1)),2),'descend');
+    else
+        % sort channels based on each channels maximum in the timewindow 
+        % better for smaller time windows
+        mxchan = max(abs(avg.avg(:,minind:maxind)),[],2);
+        [~, chans] = sort(mxchan,'descend');
+    end
+    Lchans = chans(layout.pos(chans,1) < 0);  
+    Rchans = chans(layout.pos(chans,1) > 0);
+    
+    bestchannels(:,1) = Lchans(1:nchans);
+    bestchannels(:,2) = Rchans(1:nchans);
+    
+    if plotflag
+        cfg = [];
+        cfg.layout = layout;
+        cfg.baseline = [-.3 0];
+        cfg.xlim = avg.time([minind maxind]);
+        cfg.interactive = 'yes';
+        cfg.marker = 'off';
+        cfg.style = 'fill';
+        figure;
+        subplot(211);
+        ft_topoplotER(cfg,avg);
+        hold on; scatter(layout.pos(bestchannels(:,1),1),layout.pos(bestchannels(:,1),2),'w');
+        scatter(layout.pos(bestchannels(:,2),1),layout.pos(bestchannels(:,2),2),'g');
+        subplot(2,1,2);
+        plot(avg.time,avg.avg); hold on;
+        ylim = get(gca,'ylim');
+        tindx = patch([timewin(1).*[1; 1]; timewin(2).*[1;1]], [ylim';flipud(ylim')], 'k');
+        set(tindx, 'FaceAlpha', 0.1);
+        set(gca,'ylim',ylim)
+        set(gca,'xlim',plotwin);
+    end
+end
+
